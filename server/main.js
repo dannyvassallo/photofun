c5891ddadbfd24f539695378026e3d1f8779f0c7
dannyvassallo <danielvassallo87@gmail.com>
2016-12-06 02:28:10 -0500

    updated readme and server.js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { BrowserPolicy } from 'meteor/browser-policy-common';
 
 Meteor.startup(() => {
+  BrowserPolicy.content.allowOriginForAll('*');
   // code to run on server at startup
+
+  // create admin from settings
+  if (Meteor.users.findOne(Meteor.settings.adminId)){
+    Roles.addUsersToRoles(Meteor.settings.adminId, ['admin']);
+  }
 });
